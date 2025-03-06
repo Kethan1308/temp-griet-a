1f0320532b32ed255c535fc830aa5b6d20949cc7
Kethan1308 <nukalanagakethansai@gmail.com>
2025-03-06 10:39:28 +0530

    added

---
+++ calc.js
@@ -0,0 +1,17 @@
+// export function add(a,b){ 
+    // function add(a,b){ if you want to run this just comment the 1st line
+    function add(a,b){
+    return a+b
+}
+// export default add
+
+// export function subtract(a,b){
+    function subtract(a,b){
+    return a-b
+}
+    
+     function greet(){
+        return 'Hello'
+     }
+export {add,subtract,greet}
+// We can use only one default in the function
---
+++ main.js
@@ -0,0 +1,7 @@
+import {add,subtract,greet} from "./calc.js" //here we took subtract in {} brackets because in exprot function there is no default in c
+let result = add(2,3)
+console.log(result)
+let result1=subtract(9,2)
+console.log(result1)
+let str = greet()
+console.log(str)
